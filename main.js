9a4b53085e1e3a4ea0cb7128ff0aa580bcd57794
Silas Burger <silas.a.burger@gmail.com>
2018-08-31 13:45:02 -0700

    added owl images to page

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
+function newGame() {
+  var spans = document.querySelectorAll('span');
+  var numPool = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18];
+  var chosen = [];
+  for (let i = 0; i < spans.length; i++) {
+    var random = Math.floor(Math.random() * numPool.length);
+    var pic = 'owls/' + numPool[random] + '.png';
+    spans[i].style.backgroundImage = 'URL(' + pic + ')';
+    if (chosen.includes(numPool[random])) {
+      numPool.splice(random, 1);
+    } else {
+      chosen.push(numPool[random]);
+    }
+  }
+}
+
+document.getElementById('new-game').addEventListener('click', newGame());
+
 document
   .getElementById('game-container')
   .addEventListener('click', function(e) {
