350bd941b41c237f13363c37c481f95323320fca
Silas Burger <silas.a.burger@gmail.com>
2018-09-02 20:30:58 -0700

    finishing choose()

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+newGame();
+
 function newGame() {
   var spans = document.querySelectorAll('span');
   var numPool = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18];
@@ -14,7 +16,37 @@ function newGame() {
   }
 }
 
-document.getElementById('new-game').addEventListener('click', newGame());
+function choose() {
+  var closestCard = e.target.closest('.card');
+  var closestSpan = e.target.closest('span');
+  var oneFlipped = false;
+  return function() {
+    if (closestCard.contains(e.target) && !oneFlipped) {
+      closestCard.classList.toggle('is-flipped');
+      oneFlipped = true;
+    } else if (
+      closestCard.contains(e.target) &&
+      oneFlipped &&
+      document.getElementsByClassName('is-flipped')[0].childNodes[3]
+        .childNodes[0].style.backgroundImage ===
+        closestSpan.style.backgroundImage
+    ) {
+      document.getElementsByClassName('is-flipped')[0].style.transform =
+        'rotateY(180deg)';
+      closestSpan.parentNode.parentNode.style.transform = 'rotateY(180deg)';
+      document.getElementsByClassName('is-flipped')[0].toggle('is-flipped');
+      oneFlipped = false;
+    } else if (closestCard.contains(e.target) && oneFlipped) {
+      document.getElementsByClassName('is-flipped')[0].toggle('is-flipped');
+      closestCard.toggle('is-flipped');
+      Window.setTimeout(function() {
+        closestCard.toggle('is-flipped');
+      }, 1000);
+    }
+  };
+}
+
+document.getElementById('new-game').addEventListener('click', newGame);
 
 document
   .getElementById('game-container')
